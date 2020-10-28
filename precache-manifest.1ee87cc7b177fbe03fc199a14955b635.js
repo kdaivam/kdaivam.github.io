0cedb987babe9a0870e987b07dd9afb6208b5f35
kdaivam <daivam.kanya@gmail.com>
2020-10-27 20:07:27 -0700

    Updates

---
+++ precache-manifest.1ee87cc7b177fbe03fc199a14955b635.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1e50d7b6a8a7721a96f7f93202466e8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "abc1eb0356b0bb57d01f",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "30dc0dd7a23ad8a3dc81",
+    "url": "/static/css/main.e9084563.chunk.css"
+  },
+  {
+    "revision": "abc1eb0356b0bb57d01f",
+    "url": "/static/js/2.d2590788.chunk.js"
+  },
+  {
+    "revision": "80fa05aac9f80c71b5f99ecec6ead10d",
+    "url": "/static/js/2.d2590788.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "30dc0dd7a23ad8a3dc81",
+    "url": "/static/js/main.384040ef.chunk.js"
+  },
+  {
+    "revision": "c88671eb88bf6c28dee9",
+    "url": "/static/js/runtime-main.6a060e8a.js"
+  },
+  {
+    "revision": "21c3688f153c897a4abc2b779a724d61",
+    "url": "/static/media/Profile.21c3688f.jpg"
+  },
+  {
+    "revision": "e75af3b2614658cf16167d7dd996eb6b",
+    "url": "/static/media/resume.e75af3b2.pdf"
+  }
+]);
